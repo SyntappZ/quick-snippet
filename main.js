1e1a5026374a3fa411d9335a406c6881e4d91c85
biscuitmanz <doddsanater87@gmail.com>
2018-11-20 01:11:57 +0000

    finished everything except for local storage

---
+++ main.js
@@ -0,0 +1,38 @@
+let form = document.getElementById('form');
+let input = document.getElementById('input');
+let list = document.getElementById('list');
+
+
+form.addEventListener('submit', (e) => {
+ e.preventDefault();
+  createItem(input.value);
+ 
+})
+
+// `<li>${x} <button onclick="deleteItem(this)" class="btn">X</button></li>`
+
+function createItem(x){
+  let addList = `<div id="addWrap"><div onclick="deleteItem(this)" id="trash"><i class="fas fa-trash-alt"></i></div><div onclick="lineThrough(this)" id="addItem">${x}</div></div>`
+  list.insertAdjacentHTML('beforeend', addList);
+  input.value = '';
+  input.focus();
+}
+function deleteItem(eleToDelete){
+     eleToDelete.parentElement.remove();
+}
+
+function lineThrough(line){
+    if(line.style.textDecoration === 'line-through'){
+        line.style.textDecoration = 'none';
+        line.style.transform = 'scale(1)';
+        line.style.backgroundColor = '#F0F0DF';
+    }else{
+        line.style.textDecoration = 'line-through';
+        line.style.transform = 'scale(.97)';
+        line.style.backgroundColor = '#BDD3DE';
+    }
+   
+    
+    
+    
+}

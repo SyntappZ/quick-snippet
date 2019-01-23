823d42c59957c86a85b450247094780e4bea171e
biscuitmanz <doddsanater87@gmail.com>
2019-01-23 00:23:37 +0000

    line through the text now saves in storge

--- main.js
+++ main.js
@@ -19,6 +19,12 @@ let textArray = localStorage.getItem('mainText') ? JSON.parse(localStorage.getIt
 localStorage.setItem('mainText', JSON.stringify(textArray));
 const textData = JSON.parse(localStorage.getItem('mainText'));
 
+//line storage
+let lineArray = localStorage.getItem('line') ? JSON.parse(localStorage.getItem('line')) : [];
+localStorage.setItem('line', JSON.stringify(lineArray));
+const lineData = JSON.parse(localStorage.getItem('line'));
+
+
 
 
 //add snippets to localStorage
@@ -30,6 +36,9 @@ localStorage.setItem('title', JSON.stringify(titleArray));
 
 textArray.push(textArea.value);
 localStorage.setItem('mainText', JSON.stringify(textArray));
+
+lineArray.push('a')
+localStorage.setItem('line', JSON.stringify(lineArray));
 
 createItem(input.value);
 
@@ -44,8 +53,23 @@ createItem(input.value);
 data.forEach(item =>{
     createItem(item);
 });
-
-
+lineLoop()
+function lineLoop(){
+   
+    for(let i = 0; i < lineArray.length; i++){
+        let textDiv = wraps[i].firstChild.nextSibling;
+        
+        if(lineArray[i] === "b"){
+            textDiv.style.textDecoration = 'line-through';
+            textDiv.style.transform = 'scale(.97)';
+            textDiv.style.backgroundColor = '#BDD3DE';
+        }else{
+            textDiv.style.textDecoration = 'none';
+            textDiv.style.transform = 'scale(1)';
+            textDiv.style.backgroundColor = '#fff';
+        }
+    }
+}
 
 
 //create elements
@@ -53,9 +77,12 @@ function createItem(x){
     let addList = `<div onmouseover="getTextFromArray(this)" class="addWrap"><div onclick="deleteItem(this)" class="trash"><i class="fas fa-trash-alt"></i></div><div onclick="lineThrough(this)" id="addItem">${x}</div><div onclick="quickCopy()" class="copy"><i class="fas fa-copy"></i></div></div>`
     list.insertAdjacentHTML('beforeend', addList);
     input.focus();
+   
+    
 }
   
 //snippet counter
+let lineCount = document.getElementById('line-count');
 let snippetNumber = wraps.length;
 snippetCount.innerHTML = snippetNumber;
 
@@ -68,9 +95,10 @@ function deleteItem(eleToDelete){
         if(data[i] === added){
             data.splice(i , 1);
             textArray.splice(i, 1);
+            lineArray.splice(i, 1);
             localStorage.setItem('title', JSON.stringify(data));
             localStorage.setItem('mainText', JSON.stringify(textArray));
-           
+            localStorage.setItem('line', JSON.stringify(lineArray));
         }
         document.location.reload();
     }
@@ -83,12 +111,26 @@ function getTextFromArray(thisWrap){
     for(let i = 0; i < data.length; i++){
         if(thisWrap === wraps[i]){
         index = i;
-            if(textArray[i].charAt(0) === '<'){ 
-                snippet.innerHTML = '<xmp>'+ textArray[i] +'</xmp>';
+            let lineWrap = thisWrap.firstChild.nextSibling.style.backgroundColor;
+            if(lineWrap === 'rgb(189, 211, 222)'){
+                //make done text and display
+                if(textArray[i].charAt(0) === '<'){ 
+                    snippet.innerHTML = '<xmp>'+ textArray[i] +'</xmp>';
+                }else{
+                    snippet.innerHTML = textArray[i];
+                }
             }else{
-                snippet.innerHTML = textArray[i];
+                if(textArray[i].charAt(0) === '<'){ 
+                    snippet.innerHTML = '<xmp>'+ textArray[i] +'</xmp>';
+                }else{
+                    snippet.innerHTML = textArray[i];
+                    
+                }
             }
-       
+                
+        
+            
+          
         }
     }
 }
@@ -111,7 +153,6 @@ searchBtn.addEventListener('click', ()=>{
                     snippet.innerHTML = '<xmp>'+ textArray[i] +'</xmp>';
                 }else{
                     snippet.innerHTML = textArray[i];
-                    console.log(searchInput + ' ' + wrapInput);
                 }
             }
         }
@@ -128,6 +169,7 @@ copyBtn.addEventListener('click', ()=>{
        snippet.innerHTML === 'Title not recognised!' ||
        snippet.innerHTML === 'no snippet to copy!'){
        snippet.innerHTML = 'no snippet to copy!';
+       console.log(lineArray)
     }else{
         quickCopy();
     }
@@ -137,53 +179,65 @@ copyBtn.addEventListener('click', ()=>{
 
 //copy snippet function
 function quickCopy(){ 
-    let toCopy = snippet;
-    let animCopy = document.querySelector('.copiedText');
-    let selection = window.getSelection();
-    let range = document.createRange();
-    range.selectNodeContents(toCopy);
-    selection.removeAllRanges();
-    selection.addRange(range);
-    document.execCommand("Copy");
-    animCopy.style.opacity = '1';
-    animCopy.style.transform = 'scale(2)';
-    setTimeout(() => {
-        animCopy.style.opacity = '0';
-    }, 400);
-    setTimeout(() => {
-        animCopy.style.transform = 'scale(1)';
-    }, 700);
+    if(snippet.innerHTML === 'done!'){
+        null;
+    }else{
+        let toCopy = snippet;
+        let animCopy = document.querySelector('.copiedText');
+        let selection = window.getSelection();
+        let range = document.createRange();
+        range.selectNodeContents(toCopy);
+        selection.removeAllRanges();
+        selection.addRange(range);
+        document.execCommand("Copy");
+        animCopy.style.opacity = '1';
+        animCopy.style.transform = 'scale(2)';
+        setTimeout(() => {
+            animCopy.style.opacity = '0';
+        }, 400);
+        setTimeout(() => {
+            animCopy.style.transform = 'scale(1)';
+        }, 700);
+    }
+   
   }
 
-let underlineStorage = []
+
 //line through the title  
 function lineThrough(line){
     if(line.style.textDecoration === 'line-through'){
         line.style.textDecoration = 'none';
         line.style.transform = 'scale(1)';
-        line.style.backgroundColor = '#F0F0DF';
-        
+        line.style.backgroundColor = '#fff';
+       
     }else{
         line.style.textDecoration = 'line-through';
         line.style.transform = 'scale(.97)';
         line.style.backgroundColor = '#BDD3DE';
     }
     //need to make this store in storage somehow!
-   
+    lineCheck()
+    
 }
-//underline to storage
-function storeUnderline(){
-    for(let i = 0; i < wraps.length; i++){
-        if(wraps[i].style.textDecoration === 'line-through'){
-            console.log(wraps[i])
+
+
+    function lineCheck(){
+        for(let x = 0; x < wraps.length; x++){
+            let textDiv = wraps[x].firstChild.nextSibling;
+            if(textDiv.style.backgroundColor === 'rgb(189, 211, 222)'){
+               lineArray[x] = "b";
+            }else{
+                lineArray[x] = "a";
+            }
+                     
         }
-
+        localStorage.setItem('line', JSON.stringify(lineArray));
+        
+        console.log(lineArray);
     }
+    
 
 
-
-
-}
 
 
 //remove all snippets and elements
@@ -210,6 +264,7 @@ btn.addEventListener('click', function(){
     colorPalette.addEventListener('mouseover', ()=>{
         colorChoice.style.transform = 'scale(1)';
         colorChoice.style.opacity = '1';
+      
     })
    
     // colorChoice.style.transform = 'scale(0)';
